690b903fb9621067e5486b5a8c0d012e1433e251
Prasad Patil <pgpatil614@gmail.com>
2026-07-01 11:23:04 +0530

    First python program

---
+++ first.py
@@ -0,0 +1,32 @@
+#write a program to display datatypes, memory size and SIZE in bytes 
+
+a = 11
+b = 10.5
+c = 6 + 4j
+name = "Jay Ganesh..."
+marks1 = (70,80,90,51,78)
+marks2 = [90,98,80,78,67]
+marks3 = {50,60,70,80,90}
+positive = True
+
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+print(c)
+print(type(a))
+
+print(name)
+print(type(name))
+
+print(marks1)
+print(type(marks1))
+
+print(marks2)
+print(type(marks2))
+
+print(marks3)
+print(type(marks3))
+
