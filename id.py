bacc48ff36492e4436c00958619c9293d19b9fc4
Prasad Patil <pgpatil614@gmail.com>
2026-07-01 11:27:58 +0530

    id in python

---
+++ id.py
@@ -0,0 +1,16 @@
+a = 10
+b = 10
+
+c = [10]
+d = [10]
+
+print(id(a))        # same id
+print(id(b))        # same id 
+
+print(id(c))        # Different id
+print(id(d))        # Different id
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
